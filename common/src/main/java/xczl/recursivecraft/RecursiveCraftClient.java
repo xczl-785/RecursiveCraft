3315031b056be5bd774d3b1d9ee3684d01ab6a45
xczl785 <750662297@qq.com>
2026-06-17 20:01:08 +0800

    Register client menu screen during init

--- common/src/main/java/xczl/recursivecraft/RecursiveCraftClient.java
+++ common/src/main/java/xczl/recursivecraft/RecursiveCraftClient.java
@@ -1,21 +1,15 @@
 package xczl.recursivecraft;
 
-import dev.architectury.event.events.client.ClientLifecycleEvent;
 import dev.architectury.registry.menu.MenuRegistry;
 import xczl.recursivecraft.client.RecursiveCrafterScreen;
-// 确保 Registration 已经在 common 包中
 import xczl.recursivecraft.registry.ModMenus;
 
 public class RecursiveCraftClient {
     public static void init() {
-        // Register client screens through the shared Architectury client entrypoint.
-        // Architectury 使用 MenuRegistry.registerScreenFactory
-        ClientLifecycleEvent.CLIENT_SETUP.register(instance -> {
-            MenuRegistry.registerScreenFactory(
-                    ModMenus.RECURSIVE_CRAFTER_MENU.get(),
-                    RecursiveCrafterScreen::new
-            );
-            RecursiveCraft.LOGGER.info("RecursiveCraft Client: Screen factory registered.");
-        });
+        MenuRegistry.registerScreenFactory(
+                ModMenus.RECURSIVE_CRAFTER_MENU.get(),
+                RecursiveCrafterScreen::new
+        );
+        RecursiveCraft.LOGGER.info("RecursiveCraft Client: Screen factory registered.");
     }
 }
